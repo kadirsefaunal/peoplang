84800893778c9d4451df691c6aa1c4d38d7272e0
Kadir Sefa ÜNAL <kadirsefau@gmail.com>
2018-01-08 18:17:41 +0300

    app index sayfasının kontrolleri eklendi.

--- public/js/app.js
+++ public/js/app.js
@@ -4,34 +4,35 @@ $(document).ready(function () {
     $("#postSend").click(function(){
         
         var post = {
-            content: $('#userPost').val()
+            content: $.trim($('#userPost').val())
         };
-        console.log(post);
-        if(post == null){
-            console.log("lütfen değer gir");
-        }
 
-        $.post('app/savePost', { post: post }, function (result) {
-            console.log(result);
-        });
+
+        if(post.content == null || post.content == ""){
+            toastr.error('Post can not be empty!');
+        } else {
+            $.post('app/savePost', { post: post });
+            $('#userPost').val("");
+        }
     });
 
     $("#onlineLang").change( function () {  
         var language = {
             langName: $("#onlineLang option:selected").text()
         };
-
-        console.log(language);
-
-        $.post("app/getOnline4", { language: language }, function (result) {  
-            var obj = $.parseJSON(result);
-            console.log(obj);
-            $("#onlineUsers").empty();
-            $.each(obj, function (i, item) {  
-                $("#onlineUsers").append("<div class='col-3'><a href='u/" + item.userName + "'><img src='"+ item.avatar +"' width='80' height='80'alt='...' class='rounded-circle mx-auto d-block img-fluid'></a><div class='text-center app-username'><a href='u/" + item.userName + "'>"+ item.userName +"</a><span class='app-age'> | "+ item.age +"</span> </div><div class='text-center'><img src='"+ item.flag +"' width='28px' height='20px' /><span class='app-city'> "+ item.location +" </span> </div></div>")
+        
+        if (language.langName == "Language Select") {
+            toastr.error('You can not choose this option!');
+        } else {
+            $.post("app/getOnline4", { language: language }, function (result) {  
+                var obj = $.parseJSON(result);
+                console.log(obj);
+                $("#onlineUsers").empty();
+                $.each(obj, function (i, item) {  
+                    $("#onlineUsers").append("<div class='col-3'><a href='u/" + item.userName + "'><img src='"+ item.avatar +"' width='80' height='80'alt='...' class='rounded-circle mx-auto d-block img-fluid'></a><div class='text-center app-username'><a href='u/" + item.userName + "'>"+ item.userName +"</a><span class='app-age'> | "+ item.age +"</span> </div><div class='text-center'><img src='"+ item.flag +"' width='28px' height='20px' /><span class='app-city'> "+ item.location +" </span> </div></div>")
+                });
             });
-            
-        });
+        }
     });
 
     $("#rejectFriendReq").click(function () {  
